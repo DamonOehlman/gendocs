0788e48a0a61891a3b8c966fb6b7d0cef3fda17c
Damon Oehlman <damon.oehlman@gmail.com>
2013-08-21 15:53:21 +1000

    Added additional licenses

--- plugin/license.js
+++ plugin/license.js
@@ -22,7 +22,20 @@ var licensePath = path.resolve(__dirname, '..', 'licenses');
 
   ```json
   {
-    "license": true
+    "license": {}
+  }
+  ```
+  
+  If you want to override the copyright holder (or year) from the default of
+  the author in the package.json file, then use the following in your
+  `docs.json` file:
+
+  ```json
+  {
+    "license": {
+      "year": "2008 - 2013"
+      "holder": "Foobar Corp <development@foobar.com>"
+    }
   }
   ```
 
@@ -49,8 +62,8 @@ module.exports =  pull.Through(function(read, config, pkgInfo) {
       }
 
       callback(null, '### ' + license + '\n\n' + formatter(data)({
-        year: new Date().getFullYear(),
-        pkgInfo: pkgInfo
+        year: config.year || new Date().getFullYear(),
+        holder: config.holder || pkgInfo.author
       }));
     });
   }
