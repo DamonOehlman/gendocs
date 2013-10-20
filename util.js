6faabf72a360201a3bb626e7b9fdc8951448d33b
Damon Oehlman <damon.oehlman@gmail.com>
2013-10-20 19:29:56 +1100

    Updated to include bitbucket urls project url discovery

--- util.js
+++ util.js
@@ -1,8 +1,13 @@
-var reGithubProject = /^.*github\.com\/(.*)\.git.*$/;
+var regexes = [
+  /^.*github\.com\/(.*)\.git.*$/, // github
+  /^.*bitbucket\.org\/(.*)\.git.*$/ // bitbucket
+];
 
 exports.getRepoName = function(pkgInfo) {
   var url = ((pkgInfo || {}).repository || {}).url;
-  var match = reGithubProject.exec(url);
+  var match = regexes.reduce(function(memo, regex) {
+    return memo || regex.exec(url);
+  }, false);
 
   return match ? match[1].split('/').splice(0, 2).join('/') : '';
 };
